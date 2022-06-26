d25e4c17daae1f2ebceb883539b86b2b4931d069
Jacob Britton <jacobbritton95@gmail.com>
2022-06-25 21:23:43 -0400

    Create randomly generated computer response

---
+++ main.js
@@ -0,0 +1,13 @@
+function computerPlay () {
+    let randomNum = Math.floor( Math.random() * 3 );
+
+    if (randomNum === 0) {
+        return "Rock";
+    } else if (randomNum === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(computerPlay());
