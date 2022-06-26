f8ee1cedc139682e2f538b6963065f5aa68702f7
Jacob Britton <jacobbritton95@gmail.com>
2022-06-25 21:51:29 -0400

    Create user response function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function computerPlay () {
+function computerPlay() {
     let randomNum = Math.floor( Math.random() * 3 );
 
     if (randomNum === 0) {
@@ -10,4 +10,31 @@ function computerPlay () {
     }
 }
 
-console.log(computerPlay());
+
+function playerPlay() {
+    let userResponse
+    
+    while(1) {
+        userResponse = prompt("Enter your move", "");
+    
+        if (userResponse === "" || userResponse === null) {
+            alert("Invalid response. Please try again.");
+        } else {
+            userResponse = userResponse[0].toUpperCase() 
+                            + userResponse.toLowerCase().slice(1);
+
+            if (userResponse === "Rock") {
+                return "Rock";
+            } else if (userResponse === "Paper") {
+                return "Paper";
+            } else if (userResponse === "Scissors") {
+                return "Scissors";
+            } else {
+                alert("Invalid response. Please try again");
+            }
+        }
+    }
+
+}
+
+console.log(playerPlay());
